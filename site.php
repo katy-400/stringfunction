9fdb840c9ac5afe0c898c7680e8018b071d55e4a
Katy <kn96@njit.edu>
2016-11-07 02:36:11 -0500

    htmlentities

--- site.php
+++ site.php
@@ -1,47 +1,61 @@
+<html> <h1> String Functions </h1> </html>
+
 <html> <h3> addslashes </h3> </html>
 
-<?php
-	$str = "Is your name O'Reilly?";
-
-	// Outputs: Is your name O\'Reilly?
-	echo addslashes($str);
-?>
+  <?php
+  	$str = "Is your name O'Reilly?";
+  
+  	// Outputs: Is your name O\'Reilly?
+  	echo addslashes($str);
+  ?>
 
 <html> <h3> chr </h3> </html>
 
-<?php	
-	echo chr(-159), chr(833), PHP_EOL;
-?>
+  <?php	
+  	echo chr(-159), chr(833), PHP_EOL;
+  ?>
 
 <html> <h3> chunk_split </h3> </html>
 
-<?php
-	// format $data using RFC 2045 semantics
-	$new_string = chunk_split(base64_encode($data));
-?>
+  <?php
+  	// format $data using RFC 2045 semantics
+  	$new_string = chunk_split(base64_encode($data));
+  ?>
 
 
 <html> <h3> count_chars </h3> </html>
 
-<?php	
-	$data = "My name Starkesha.";
-
- 	foreach (count_chars($data, 1) as $i => $val) {
-    		echo "There were $val instance(s) of \"" , chr($i) , "\" in the string.\n";
-    }
-?>
+  <?php	
+  	$data = "My name Starkesha.";
+  
+   	foreach (count_chars($data, 1) as $i => $val) {
+      		echo "There were $val instance(s) of \"" , chr($i) , "\" in the string.\n";
+      }
+  ?>
 
 <html> <h3> explode </h3> </html>
 
-<?php
-  // Example 1
-  $pizza  = "piece1 piece2 piece3 piece4 piece5 piece6";
-  $pieces = explode(" ", $pizza);
-  echo $pieces[0]; // piece1
-  echo $pieces[1]; // piece2
-?>
+  <?php
+    // Example 1
+    $pizza  = "piece1 piece2 piece3 piece4 piece5 piece6";
+    $pieces = explode(" ", $pizza);
+    echo $pieces[0]; // piece1
+    echo $pieces[1]; // piece2
+  ?>
 
 <html> <h3> htmlentities </h3> </html>
+
+  
+  <?php
+    $str = "A 'quote' is <b>bold</b>";
+    
+    // Outputs: A 'quote' is &lt;b&gt;bold&lt;/b&gt;
+    echo htmlentities($str);
+    
+    // Outputs: A &#039;quote&#039; is &lt;b&gt;bold&lt;/b&gt;
+    echo htmlentities($str, ENT_QUOTES);
+  ?>
+
 
 <html> <h3> htmlspecialchars_decode </h3> </html>
 
